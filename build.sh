664f17beae7b10be0aed9692201f12cbc95c6418
giacomo-c <giacomo.carissimi@protonmail.com>
2025-12-11 14:24:51 -0500

    update: remove powerpanel rpm from build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ dnf5 install -y \
     qt6-qtwebsockets-devel \
 
 # install powerpanel for CyberPower UPS
-dnf5 install -y https://dl4jz3rbrsfum.cloudfront.net/software/PPL_64bit_v1.4.1.rpm
+# dnf5 install -y https://dl4jz3rbrsfum.cloudfront.net/software/PPL_64bit_v1.4.1.rpm
 
 # install latest rclone
 dnf remove -y rclone
